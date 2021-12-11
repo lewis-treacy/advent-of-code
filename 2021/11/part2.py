97eec6759a6613d65819926c6ad2c73b1e2c7241
Lewis Treacy <lewisjtreacy@gmail.com>
2021-12-11 15:02:32 +0100

    Add day 11 solution

---
+++ 2021/11/part2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+def get_adjacent(grid, x, y):
+    adjacents = set()
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if x+i >= 0 and x+i <= len(grid[0])-1 and y+j >= 0 and y+j <= len(grid)-1:
+                adjacents.add((x+i,y+j))
+    return adjacents
+
+def should_flash(grid, x, y, flashed):
+    if grid[y][x] > 9 and (x, y) not in flashed:
+        flashed.add((x,y))
+        adjacents = get_adjacent(grid, x, y)
+        for a, b in adjacents:
+            grid[b][a] += 1
+        for a, b in adjacents:
+            should_flash(grid, a, b, flashed)
+    return flashed
+
+with open("input.txt", "r") as f:
+    grid = [[int(x) for x in l.rstrip()] for l in f]
+octopuses = sum(len(l) for l in grid)
+
+step = 0
+while True:
+    step += 1
+    flashed = set()
+    for y in range(0, len(grid)):
+        for x in range(0, len(grid[y])):
+            grid[y][x] += 1
+            should_flash(grid, x, y, flashed)
+
+    for x, y in flashed:
+        grid[y][x] = 0
+
+    if len(flashed) == octopuses:
+        print(step)
+        break
+
+
