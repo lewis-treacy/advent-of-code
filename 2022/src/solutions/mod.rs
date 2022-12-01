1efc47daa5bd268826e8639485eeb41942f40242
Lewis Treacy <lewisjtreacy@gmail.com>
2022-12-01 19:02:25 +0000

    Add day 1 solution

--- 2022/src/solutions/mod.rs
+++ 2022/src/solutions/mod.rs
@@ -1,7 +1,11 @@
 use crate::solver::Solver;
 
+mod day01;
+
 pub fn exec_day(day: u32) {
     match day {
+        1 => day01::Problem {}.solve(day),
+
         d => println!("Day {d} hasn't been solved yet :("),
     }
 }
